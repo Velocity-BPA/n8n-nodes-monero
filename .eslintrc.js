bf7629b9111099d6a28e52c36a31c121920d0984
John Drefahl <john@velobpa.com>
2026-02-24 05:48:21 -0600

    chore: update .eslintrc.js to v1.0.0

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,15 @@
+/*
+ * Copyright (c) 2026 Velocity BPA, LLC
+ * Licensed under the Business Source License 1.1
+ */
+
 module.exports = {
-	root: true,
-	env: {
-		es6: true,
-		node: true,
-	},
-	parser: '@typescript-eslint/parser',
-	parserOptions: {
-		project: ['./tsconfig.json'],
-		sourceType: 'module',
-		ecmaVersion: 2020,
-	},
-	ignorePatterns: [
-		'.eslintrc.js',
-		'**/*.js',
-		'**/node_modules/**',
-		'**/dist/**',
-		'**/test/**',
-	],
-	plugins: ['@typescript-eslint', 'n8n-nodes-base'],
-	extends: ['plugin:n8n-nodes-base/community'],
-	rules: {
-		'n8n-nodes-base/node-dirname-against-convention': 'off',
-	},
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['eslint-plugin-n8n-nodes-base'],
+  extends: ['plugin:n8n-nodes-base/community'],
+  ignorePatterns: ['node_modules/**', 'dist/**'],
+  rules: {
+    'n8n-nodes-base/community-package-json-name-still-default': 'off',
+  },
 };
